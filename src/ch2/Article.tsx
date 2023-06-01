f36a5bd20b56e6c258163aa6d1fbc81a862417a7
Juntao Qiu <juntao.qiu@gmail.com>
2023-06-01 20:56:55 +1000

    added code examples to ch2

---
+++ src/ch2/Article.tsx
@@ -0,0 +1,11 @@
+type ArticleType = { heading: string; summary: string };
+const Article = ({ heading, summary }: ArticleType) => {
+  return (
+    <article>
+      <h3>{heading}</h3>
+      <p>{summary}</p>
+    </article>
+  );
+};
+
+export default Article;
