96501d19a749c28af98d7884c783f68d229b0165
Juntao Qiu <juntao.qiu@gmail.com>
2023-06-02 11:15:22 +1000

    first draft of ch2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
-import Login from "./ch2/Login";
+import ThemeProvider from "./ch2/ThemeProvider";
+import Article from "./ch2/Article";
 
 function App() {
   return (
     <div className="app">
-      <Login />
+      <ThemeProvider>
+        <Article
+          heading="Think in components"
+          summary="It's important to change your mindset when coding with React."
+        />
+
+        <Article
+          heading="Define custom hooks"
+          summary="Hooks are a great way to share state logic."
+        />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/ch2/ArticleEditor.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useCallback, ChangeEvent } from "react";
+
+const ArticleEditor = ({ id }: { id: string }) => {
+  const [summary, setSummary] = useState("");
+
+  const submitChange = useCallback(async () => {
+    try {
+      // Make a POST request to update the article item
+      await fetch(`/api/articles/${id}`, {
+        method: "POST",
+        body: JSON.stringify({ summary }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      // handling errors
+    }
+  }, [id, summary]);
+
+  const handleSummaryChange = useCallback(
+    (event: ChangeEvent<HTMLTextAreaElement>) => {
+      setSummary(event.target.value);
+    },
+    []
+  );
+
+  return (
+    <div>
+      <h2>Edit Article</h2>
+      <textarea value={summary} onChange={handleSummaryChange} />
+      <button onClick={submitChange}>Save</button>
+    </div>
+  );
+};
+
+export default ArticleEditor;
