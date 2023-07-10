769e22c03091cf8d74b46054ff93f8582fd09440
Juntao Qiu <juntao.qiu@gmail.com>
2023-07-10 15:14:51 +1000

    refactored the page component

--- src/ch4/Page.tsx
+++ src/ch4/Page.tsx
@@ -1,42 +1,83 @@
 import React from "react";
 
-type PageProps = {
-  headerTitle: string;
-  headerSubtitle: string;
-  sidebarLinks: string[];
-  isLoading: boolean;
-  mainContent: React.ReactNode;
-  onHeaderClick: () => void;
-  onSidebarLinkClick: (link: string) => void;
+type HeaderProps = {
+  title: string;
+  subtitle: string;
+  onClick: () => void;
 };
 
-function Page({
-  headerTitle,
-  headerSubtitle,
-  sidebarLinks,
-  mainContent,
-  isLoading,
-  onHeaderClick,
-  onSidebarLinkClick,
-}: PageProps) {
+const Header = ({ title, subtitle, onClick }: HeaderProps) => {
+  return (
+    <header onClick={onClick}>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+    </header>
+  );
+};
+
+type SidebarProps = {
+  links: string[];
+  onLinkClick: (link: string) => void;
+};
+
+const Sidebar = ({ links, onLinkClick }: SidebarProps) => {
+  return (
+    <aside>
+      <ul>
+        {links.map((link) => (
+          <li key={link} onClick={() => onLinkClick(link)}>
+            {link}
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+type MainProps = {
+  isLoading: boolean;
+  content: React.ReactNode;
+};
+
+const Main = ({ isLoading, content }: MainProps) => {
+  return <>{!isLoading && <main>{content}</main>}</>;
+};
+
+type PageProps = {
+  header: React.ReactNode;
+  sidebar: React.ReactNode;
+  main: React.ReactNode;
+};
+
+function Page({ header, sidebar, main }: PageProps) {
   return (
     <div>
-      <header onClick={onHeaderClick}>
-        <h1>{headerTitle}</h1>
-        <h2>{headerSubtitle}</h2>
-      </header>
-      <aside>
-        <ul>
-          {sidebarLinks.map((link) => (
-            <li key={link} onClick={() => onSidebarLinkClick(link)}>
-              {link}
-            </li>
-          ))}
-        </ul>
-      </aside>
-      {!isLoading && <main>{mainContent}</main>}
+      {header}
+      {sidebar}
+      {main}
     </div>
   );
 }
+
+const MyPage = () => {
+  return (
+    <Page
+      header={
+        <Header
+          title="My application"
+          subtitle="Product page"
+          onClick={() => console.log("toggle header")}
+        />
+      }
+      sidebar={
+        <Sidebar
+          links={["Home", "About", "Contact"]}
+          onLinkClick={() => console.log(`toggle sidebar`)}
+        />
+      }
+      main={<Main isLoading={false} content={<div>The main</div>} />}
+    />
+  );
+};
 
 export default Page;
