fc9102cab327f182420f0f0f76b618c63c31ad36
Juntao Qiu <juntao.qiu@gmail.com>
2023-09-02 21:10:44 +1000

    added chapter 7

--- src/ch5/TermsAndConditions.tsx
+++ src/ch5/TermsAndConditions.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 const TheLegalContent = () => {
   return (
@@ -7,6 +7,39 @@ const TheLegalContent = () => {
     </p>
   );
 };
+
+type CheckBoxProps = {
+  label: string;
+  isChecked: boolean;
+  onCheck: (event: any) => void
+}
+
+const CheckBox = ({label, isChecked, onCheck}: CheckBoxProps) => {
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={onCheck}
+      />
+      {label}
+    </label>
+  )
+}
+
+type ButtonProps = {
+  type: 'standard' | 'primary' | 'secondary';
+  label: string;
+  disabled?: boolean;
+}
+
+const Button = ({label, disabled = true}: ButtonProps) => {
+  return (
+    <div style={{margin: '0.5rem 0'}}>
+      <button disabled={disabled}>{label}</button>
+    </div>
+  )
+}
 
 const UserConsent = () => {
   const [isChecked, setIsChecked] = useState(false);
@@ -17,18 +50,8 @@ const UserConsent = () => {
 
   return (
     <>
-      <label>
-        <input
-          type="checkbox"
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-        />
-        I accept the terms and conditions
-      </label>
-
-      <div style={{margin: '0.5rem 0'}}>
-        <button disabled={!isChecked}>Next</button>
-      </div>
+      <CheckBox isChecked={isChecked} onCheck={handleCheckboxChange} label="I accept the terms and conditions" />
+      <Button type="primary" label="Next" disabled={!isChecked} />
     </>
   );
 };
@@ -37,10 +60,10 @@ const TermsAndConditions = () => {
   return (
     <div>
       <h2>Terms and Conditions</h2>
-      <TheLegalContent />
-      <UserConsent />
+      <TheLegalContent/>
+      <UserConsent/>
     </div>
   );
 };
 
-export { TermsAndConditions };
+export {TermsAndConditions};
---
+++ src/ch7/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const pizzas = [
+  "Margherita Pizza",
+  "Pepperoni Pizza",
+  "Veggie Supreme Pizza",
+  "Chicken BBQ Pizza",
+  "Spicy Meat Feast Pizza",
+  "Pasta Primavera",
+  "Caesar Salad",
+  "Chocolate Lava Cake",
+];
+
+const MenuList = ({
+  onAddMenuItem,
+}: {
+  onAddMenuItem: (item: string) => void;
+}) => {
+  return (
+    <div data-testid="menu-list">
+      <ol>
+        {pizzas.map((item) => (
+          <li key={item}>
+            {item}
+            <button onClick={() => onAddMenuItem(item)}>Add</button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+const ShoppingCart = ({ cartItems }: { cartItems: string[] }) => {
+  return (
+    <div data-testid="shopping-cart">
+      <ol>
+        {cartItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ol>
+      <button disabled={cartItems.length === 0}>Place My Order</button>
+    </div>
+  );
+};
+
+export function PizzaShopApp() {
+  const [cartItems, setCartItems] = useState<string[]>([]);
+
+  const addItem = (item: string) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  return (
+    <>
+      <h1>The Code Oven</h1>
+      <MenuList onAddMenuItem={addItem} />
+      <ShoppingCart cartItems={cartItems} />
+    </>
+  );
+}
